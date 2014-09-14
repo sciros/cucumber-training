a49d5617d9a75bd70442aab40b778280f21123c1
Dmitry Sharkov <dsharkov@gmail.com>
2014-09-14 11:52:03 -0400

    added another todo to movies class

--- features/pages/movies.rb
+++ features/pages/movies.rb
@@ -3,5 +3,6 @@ require_relative 'base_page'
 class Movies < BasePage
   page_url($base_url+'/movies')
 
+  #TODO since 'movies' is now 'view_movies' it's less about hiding and more about being descriptive
   div(:movie_list, :id => 'movies_accordion') #dont hide 'movies' in base page
 end
