593fbec3bfc3a9c10e391060118ce45eadc36f5a
Dmitry Sharkov <dsharkov@gmail.com>
2015-05-31 15:10:28 -0400

    replaced global with constant

--- features/pages/login.rb
+++ features/pages/login.rb
@@ -1,7 +1,7 @@
 require_relative 'base_page'
 
 class Login < BasePage
-  page_url($base_url+'/login')
+  page_url(BASE_URL + '/login')
 
   text_field(:username, :id => 'username')
   text_field(:password, :id => 'password')
