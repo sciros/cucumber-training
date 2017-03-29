7d268c1e06bc8e62611a78cd04114805d22067d5
Dmitry Sharkov <dsharkov@gmail.com>
2017-03-29 13:37:58 -0400

    added capabilities for chrome to start properly (maybe temporary)

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,11 @@
 require 'watir'
 
-browser = Watir::Browser.new :chrome
+browser_arguments = { detach:true }
+chrome_options = { 'args' => %w(disable-web-security start-maximized disable-infobars) }
+capabilities = Selenium::WebDriver::Remote::Capabilities.chrome('chromeOptions' => chrome_options)
+browser_arguments[:desired_capabilities] = capabilities
+
+browser = Watir::Browser.new :chrome, browser_arguments
 
 this_file_location = File.dirname __FILE__
 config_file_path = this_file_location + '/../../config/config.yml' #directly in project directory
