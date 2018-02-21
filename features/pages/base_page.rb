fc1ab3791cbc9369a7c20afb711f1af9c615c19d
Dmitry Sharkov <dsharkov@gmail.com>
2018-02-21 10:29:56 -0500

    moved stuff around in base_page

--- features/pages/base_page.rb
+++ features/pages/base_page.rb
@@ -1,13 +1,13 @@
 class BasePage
   include PageObject
 
+  link(:log_out, visible_text: /Logout/)
+  link(:view_movies, visible_text: 'Movies')
+  link(:view_theaters, visible_text: 'Theaters')
+
   def should_contain_text(text, error_message="Expected text '#{text}' never appeared")
     wait_until(5, error_message) do
       self.text.include? text
     end
   end
-
-  link(:log_out, visible_text: /Logout/)
-  link(:view_movies, visible_text: 'Movies')
-  link(:view_theaters, visible_text: 'Theaters')
 end
